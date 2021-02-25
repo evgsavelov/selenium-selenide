90518061e3b5371718ed5edeb5035df7a1e4fdc0
evgsavelov <savelov08@ya.ru>
2021-02-25 17:35:29 +0300

    chore: initial commit

---
+++ src/test/java/ru/netology/CardOrder.java
@@ -0,0 +1,21 @@
+package ru.netology;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardOrder {
+    @Test
+    void shouldSubmitRequest() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Васильев Василий");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $(".Success_successBlock__2L3Cw").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
